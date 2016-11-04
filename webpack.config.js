d62a978a89bcf534d880ccbb7409836ed4fae2e2
aaron <401545553@qq.com>
2016-11-04 09:24:35 +0800

    add a new project for learn

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+    entry: ['webpack/hot/dev-server', path.resolve(__dirname, './app/main.js')],
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: 'bundle.js',
+    }
+}
